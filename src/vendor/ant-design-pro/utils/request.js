062ab490b2e8acfc3536a049fb3d2794f33b8d5c
wkl505997900 <505997900@qq.com>
2023-05-10 16:22:48 +0800

    修改配置文件

--- src/vendor/ant-design-pro/utils/request.js
+++ src/vendor/ant-design-pro/utils/request.js
@@ -57,10 +57,10 @@ const errorHandler = error => {
 
 // request interceptor
 request.interceptors.request.use(config => {
-  const token = localStorage.getItem('Access-Token').substring(1, localStorage.getItem('Access-Token').length - 1)
   // 如果 token 存在
   // 让每个请求携带自定义 token 请根据实际情况自行修改
-  if (token) {
+  if (localStorage.getItem('Access-Token')) {
+    const token = localStorage.getItem('Access-Token').substring(1, localStorage.getItem('Access-Token').length - 1)
     config.headers['Access-Token'] = token;
 
     // reset cookie expired time
